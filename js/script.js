279755db7cb1398cd3c654dffed00105391f6497
Suelen Braga <suelen.braga2006@gmail.com>
2017-02-01 20:08:58 -0200

    Efeito promoscope terminado

--- js/script.js
+++ js/script.js
@@ -22,5 +22,8 @@ $(window).scroll(function(){
 
 	if(wScroll > $('.large-window').offset().top - $(window).height()){
 		$('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+
+		var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+		$('.window-tint').css({'opacity': opacity});
 	}
 })
